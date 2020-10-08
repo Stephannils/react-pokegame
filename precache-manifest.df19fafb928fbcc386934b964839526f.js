b1c0fa3fe8985e170296685990dd22861f4158cd
Stephannils <jehle.stephan@yahoo.de>
2020-10-08 05:18:44 +0200

    test upload of built react app

---
+++ precache-manifest.df19fafb928fbcc386934b964839526f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ab4fbe8e4c4c4777dbd1e27e4a5721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89949228e211730d4d69",
+    "url": "/static/css/main.8026d056.chunk.css"
+  },
+  {
+    "revision": "6e1a8981d35a0bd3ee40",
+    "url": "/static/js/2.e0b89d45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0b89d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89949228e211730d4d69",
+    "url": "/static/js/main.2c580bd1.chunk.js"
+  },
+  {
+    "revision": "250103716dfc89e5abb8",
+    "url": "/static/js/runtime-main.9ff9079d.js"
+  }
+]);
